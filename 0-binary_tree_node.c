3449f3c706d0b39d52f467ae4901d334e8ded26f
dideey <bradleyjd32@gmail.com>
2023-10-31 17:30:49 +0300

    error handling

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,23 +3,22 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * parent: the parent node
- * value:the integer to be stored
- * return - pointer to newnode or null
- * 
-*/
+ * @parent: the parent node
+ * @value:the integer to be stored
+ * Return: pointer to newnode or null
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode;
-    newnode = malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-        return (NULL);
+binary_tree_t *newnode;
+newnode = malloc(sizeof(binary_tree_t));
+if (newnode == NULL)
+return (NULL);
 
-    newnode-> n = value;
-    newnode->parent = parent;
-    newnode-> left = NULL;
-    newnode-> right = NULL;
-    return (newnode);
+newnode->n = value;
+newnode->parent = parent;
+newnode->left = NULL;
+newnode->right = NULL;
+return (newnode);
 
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_insert_left - adds a node on the left child
- * parent: the node to be added upon
- * value: the value to be added
- * return: null or pointer to added node
+* binary_tree_insert_left - adds a node on the left child
+* @parent: the node to be added upon
+* @value: the value to be added
+* Return: null or pointer to added node
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-    return(NULL);
-    binary_tree_t *node;
-    node = binary_tree_node(parent, value);
-    if (node == NULL)
-    return(NULL);
-    if(parent->left != NULL)
-    {
-        node->left = parent->left;
-        parent->left->parent = node;
+if (parent == NULL)
+return (NULL);
 
-    }
-    parent->left = node;
-    return(node);
+binary_tree_t *node;
+
+node = binary_tree_node(parent, value);
+if (node == NULL)
+	return (NULL);
+if (parent->left != NULL)
+{
+	node->left = parent->left;
+	parent->left->parent = node;
+}
+	parent->left = node;
+	return (node);
 }
