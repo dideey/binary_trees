095d1159769e9914c6740d732f77f4b425ca0bc3
dideey <bradleyjd32@gmail.com>
2023-10-31 17:52:38 +0300

    right side

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,6 +8,7 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+
 if (parent == NULL)
 return (NULL);
 
@@ -15,12 +16,12 @@ binary_tree_t *node;
 
 node = binary_tree_node(parent, value);
 if (node == NULL)
-	return (NULL);
+return (NULL);
 if (parent->left != NULL)
 {
-	node->left = parent->left;
-	parent->left->parent = node;
+newNode->left = parent->left;
+parent->left->parent = node;
 }
-	parent->left = node;
-	return (node);
+parent->left = node;
+return (node);
 }
