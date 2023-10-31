9a6453e61e4dd74c724105b7416dfca779a1b8e3
dideey <bradleyjd32@gmail.com>
2023-10-31 21:25:40 +0300

    error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,10 +8,10 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+binary_tree_t *node;
+
 if (parent == NULL)
 return (NULL);
-
-binary_tree_t *node;
 
 node = binary_tree_node(parent, value);
 if (node == NULL)
