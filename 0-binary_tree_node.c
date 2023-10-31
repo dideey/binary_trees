9287fe64f8db0f44a07d20759390f4dd5996845c
dideey <bradleyjd32@gmail.com>
2023-10-31 13:58:48 +0300

    creating node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * allocate space for node
+ * 
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    newnode = malloc(size of(parent));
+    newnode-> item = value
+    newnode-> left = NULL
+    newnode-> right = NULL
+}
---
+++ binary_trees.h
@@ -0,0 +1,6 @@
+#ifndef binary_trees
+#define binary_trees
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
