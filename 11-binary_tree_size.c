260207f526c59d585b10937b31463f68f7359a71
dideey <bradleyjd32@gmail.com>
2023-11-01 20:53:25 +0300

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size- calculates zise of tree
+ * @tree:root node
+ * Return:size
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
