b978fa48a579ed24bd1b295e997a9fab964e6aa9
dideey <bradleyjd32@gmail.com>
2023-11-01 21:13:11 +0300

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - counts leaves in a tree
+ * @tree:root node of tree
+ * Return:0 or count
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+else
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
